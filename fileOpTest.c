64a33f9356f8b48567260574e24941454536f264
root <root@parkinson-15.wot.ece.northwestern.edu>
2015-03-06 18:08:08 -0600

    1. open read write close file operation written and intercepted in hyper file api

---
+++ fileOpTest.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+  char ourFile[] = "\ourtest.txt";
+  if(strcmp(ourFile, argv[1]) == 0)
+  {
+    printf("In our file, \n");
+    int localFd = open(path, fla
+    
